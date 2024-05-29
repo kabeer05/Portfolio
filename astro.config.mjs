39213b041b945721036838113c6fda98b05bbb2d
Kabeer Arora <kabeerarora13@gmail.com>
2024-05-29 17:12:44 +0530

    config: Added support for mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon({
-      iconDir: "./src/assets/icons",
-    }),
-  ],
+  integrations: [tailwind(), icon({
+    iconDir: "./src/assets/icons"
+  }), mdx()]
 });
