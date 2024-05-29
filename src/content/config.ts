c0a553a9a645055a180f620695c21c941052cef6
Kabeer Arora <kabeerarora13@gmail.com>
2024-05-29 22:11:11 +0530

    core: Added content collection for projects and blogs

---
+++ src/content/config.ts
@@ -0,0 +1,35 @@
+import { z, defineCollection } from "astro:content";
+
+// const blogsCollection = defineCollection({
+//   type: "content",
+//   schema: ({ image }) =>
+//     z.object({
+//       title: z.string(),
+//       image: image().optional(),
+//       tags: z.array(z.string()),
+//       author: z.array(z.object({ name: z.string(), url: z.string() })),
+//       date: z.date(),
+//     }),
+// });
+
+const projectsCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      links: z
+        .object({
+          github: z.string().optional(),
+          demo: z.string().optional(),
+        })
+        .strict(),
+      image: image().optional(),
+      date: z.string().transform((date) => new Date(date)),
+      featured: z.boolean(),
+    }),
+});
+
+export const collections = {
+  projects: projectsCollection,
+};
